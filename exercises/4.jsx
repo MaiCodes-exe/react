c6865d2a7782db62fde2a5cdc2eb1719807392aa
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-25 20:09:46 +0000

    4th done

--- exercises/4.jsx
+++ exercises/4.jsx
@@ -1,2 +1,10 @@
-// Render HTML Elements to the DOM
+// // // Render HTML Elements to the DOM
 
+// // const JSX = (
+// //   <div>
+// //     <h1>Hello World</h1>
+// //     <p>Lets render this to the DOM</p>
+// //   </div>
+// // );
+// // // Change code below this line
+// // ReactDOM.render(JSX, document.getElementById("challenge-node"));
