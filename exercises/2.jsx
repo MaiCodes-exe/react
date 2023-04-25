2fc15985ad9e58b39d3cbcc1ef547ebf80afdcb5
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-25 20:03:29 +0000

    2nd done

---
+++ exercises/2.jsx
@@ -0,0 +1,12 @@
+// Create a Complex JSX Element
+// const JSX =
+// <div>
+// <h1> </h1>
+// <p> </p>
+// <ul>
+// <li> asdfgh</li>
+// <li> azsdfgh</li>
+// <li> asdfgvbh</li>
+
+// </ul>
+// </div>
