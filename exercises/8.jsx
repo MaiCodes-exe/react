75a49996112d9c7ed117dca672a106235c5fdda0
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-25 20:25:29 +0000

    8th done

--- exercises/8.jsx
+++ exercises/8.jsx
@@ -1,1 +1,17 @@
-// Create a React Component
+// // Create a React Component
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     // Change code below this line
+
+// return(
+// <div> 
+// <h1>Hello React!</h1>
+// </div>
+// );
+
+//     // Change code above this line
+//   }
+// };
