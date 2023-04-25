392d80d7ea20b46adb34d592555c859b38dd8d15
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-25 20:05:19 +0000

    3rd done

--- exercises/3.jsx
+++ exercises/3.jsx
@@ -1,7 +1,11 @@
-const JSX = (
-  <div>
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-  {/*this is a comment*/}
-  </div>
-);
+// Add Comments in JSX
+
+
+
+// const JSX = (
+//   <div>
+//     <h1>This is a block of JSX</h1>
+//     <p>Here's a subtitle</p>
+//   {/*this is a comment*/}
+//   </div>
+// );
