1beb24c964aa0351509fda2226acfa52c7eb8cba
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-30 17:15:37 +0000

    32nd done

---
+++ exercises/32.jsx
@@ -0,0 +1,15 @@
+// // Use the Lifecycle Method componentWillMount
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   componentWillMount() {
+//     // Change code below this line
+//   <div>asdfghj </div>
+//   console.log()
+//     // Change code above this line
+//   }
+//   render() {
+//     return <div />
+//   }
+// };
