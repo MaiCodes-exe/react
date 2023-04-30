7cd62a16652e876e2d89ad0dcb7dfcdd81aa3931
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-30 19:56:32 +0000

    40th done

--- exercises/40.jsx
+++ exercises/40.jsx
@@ -1,1 +1,31 @@
-// Use && for a More Concise Conditional
+// // Use && for a More Concise Conditional
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       display: true
+//     }
+//     this.toggleDisplay = this.toggleDisplay.bind(this);
+//   }
+//   toggleDisplay() {
+//     this.setState(state => ({
+//       display: !state.display
+//     }));
+//   }
+//   render() {
+//     // Change code below this line
+//     { this.state.display && 
+//           <div>
+//            <button onClick={this.toggleDisplay}>Toggle Display</button>
+//            <h1>Displayed!</h1>
+//          </div>}
+      
+//     return (
+//        <div>
+//          <button onClick={this.toggleDisplay}>Toggle Display</button>
+//          {this.state.display && <h1>Displayed!</h1>}
+
+//        </div>
+//     );
+//   }
+// };
