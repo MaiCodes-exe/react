da5738506cce8c907bcde4ceb4fbb783c75d0374
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-01 19:27:26 +0000

    47th done

--- exercises/47.jsx
+++ exercises/47.jsx
@@ -1,1 +1,12 @@
-// Render React on the Server with renderToString
+// // Render React on the Server with renderToString
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return <div/>
+//   }
+// };
+
+// // Change code below this line
+// ReactDOMServer.renderToString(<App />);
